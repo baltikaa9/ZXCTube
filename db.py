23479eb07bc3bad28b0ab4ca5171e8cfeda7d9c0
baltikaa9 <sergey4016@mail.ru>
2023-07-27 05:47:56 +0300

    init

--- db.py
+++ db.py
@@ -2,8 +2,6 @@ from typing import Generator
 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-# metadata = sqlalchemy.MetaData()
-# db = databases.Database('sqlite://zxctube.db')
 
 engine = create_async_engine('sqlite+aiosqlite:///zxctube.db')
 async_session = async_sessionmaker(engine, expire_on_commit=False)
