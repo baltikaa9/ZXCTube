5b86541623e85d2d424a2a3f2bcc29ac53a62f27
baltikaa9 <sergey4016@mail.ru>
2023-07-29 13:18:10 +0300

    [upd] renamed crud modules

--- db/session.py
+++ db/session.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-DB_URL = 'sqlite+aiosqlite:///zxctube.db'
+from config import DB_URL
 
 engine = create_async_engine(DB_URL)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
