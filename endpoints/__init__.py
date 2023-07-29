20c5256c098eb843cd1993b0eedbaf97491132d7
baltikaa9 <sergey4016@mail.ru>
2023-07-29 11:17:23 +0300

    [upd] endpoints __init__

---
+++ endpoints/__init__.py
@@ -0,0 +1,3 @@
+from .user import router as user_router
+from .video import router as video_router
+from .subscribe import router as subscribe_router
--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import uvicorn
 from fastapi import FastAPI
 from starlette.requests import Request
 
-from endpoints import user, video, subscribe
+from endpoints import user_router, video_router, subscribe_router
 from endpoints.video import templates
 
 app = FastAPI()
 
-app.include_router(video.router, prefix='/video', tags=['Video'])
-app.include_router(user.router, prefix='/user', tags=['User'])
-app.include_router(subscribe.router, prefix='/subscribe', tags=['Subscribe'])
+app.include_router(video_router, prefix='/video', tags=['Video'])
+app.include_router(user_router, prefix='/user', tags=['User'])
+app.include_router(subscribe_router, prefix='/subscribe', tags=['Subscribe'])
 
 
 @app.get('/404')
