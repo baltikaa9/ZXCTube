bcf34264149b8f8d726d18d6126b31ce380a6172
baltikaa9 <sergey4016@mail.ru>
2023-08-18 03:11:05 +0300

    catch exceptions

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from loguru import logger
 
-from backend import api_router
 from config import HOST
-from frontend import fr_router
 
 app = FastAPI(title='ZXCTube')
 
+try:
+    from backend import api_router
+    app.include_router(api_router)
+except Exception as e:
+    logger.exception(e)
+try:
+    from frontend import fr_router
+    app.include_router(fr_router)
+except Exception as e:
+    logger.exception(e)
 
-app.include_router(api_router)
-app.include_router(fr_router)
 
 app.mount('/frontend/static', StaticFiles(directory='frontend/static'), name='static')
 
 
 if __name__ == '__main__':
+    logger.add('logs.log', enqueue=True, serialize=True)
     uvicorn.run('main:app', port=8000, host=HOST)
